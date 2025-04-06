3367ff337aedd67f5d5c31d94ecb1b61b2fb5e5b
Sreenithi <151170814+SreenithiRamesh@users.noreply.github.com>
2025-04-06 19:52:12 +0530

    Update img.js

--- src/assests/img.js
+++ src/assests/img.js
@@ -3,7 +3,7 @@ export const LogoImg =
 
   export const HamburgerImg="https://ik.imagekit.io/ytissbwn8/image.png?updatedAt=1712465721968";
 
-  export const mycartimg="https://ik.imagekit.io/ytissbwn8/Screenshot%202024-04-07%20112532.png?updatedAt=1712469358108";
+  export const mycartimg="https://ik.imagekit.io/ytissbwn8/Screenshot%202024-04-07%20112532.webp?updatedAt=1743949232741";
   export const Instaimg="https://ik.imagekit.io/ytissbwn8/instagram%20(1).webp?updatedAt=1743948518711";
   export const fbImg="https://ik.imagekit.io/ytissbwn8/facebook.webp?updatedAt=1743949233387";
   export const twitterImg="https://ik.imagekit.io/ytissbwn8/twitter.webp?updatedAt=1743949232957";
